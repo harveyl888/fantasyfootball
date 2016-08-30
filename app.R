0be33b93e4366daf9908bf9491abed930de6229d
Harvey Lieberman <harvey.lieberman@gmail.com>
2016-08-29 20:52:33 -0400

    added picked players table

--- app.R
+++ app.R
@@ -33,8 +33,20 @@ server <- function(input, output) {
       incProgress(0.5, message = 'Grabbing Player Details')
       fpl$all <- fplR::getFPLData()
       fpl$currentWeek <- which(fpl$all$events$is_current)
+      
+#       currentDeadline <- as.Date(fpl$all$events[which(fpl$all$events$is_current), 'deadline_time'])
+#       if (as.numeric(difftime(Sys.Date(), currentDeadline)) == 0) {
+#         fpl$currentWeek <- which(fpl$all$events$is_current) - 1
+#       } else {
+#         fpl$currentWeek <- which(fpl$all$events$is_current)
+#       }
+      
       incProgress(1.0, message = 'Grabbing League Table')
-      fpl$league <- fplR::getLeagueTable(leagueID, fpl$currentWeek)
+      fpl$league <- fplR::getLeagueTable(leagueID, NULL)
+#      fpl$league <- fplR::getLeagueTable(leagueID, fpl$currentWeek)
+      
+#      if (length(fpl$league$standings$results) < 1)  fpl$league <- fplR::getLeagueTable(leagueID, fpl$currentWeek - 1)
+
       entries <- fpl$league$standings$results$entry
       setProgress(0, message = 'Grabbing Teams')
       l.teams <- list()
@@ -49,10 +61,12 @@ server <- function(input, output) {
                                  arrange(element_type, team) %>%
                                  mutate(element_type = .posID(element_type)) %>%
                                  mutate(team = .teamID(fpl$all, team)) %>%
+                                 rename(P = position) %>%
                                  rename(pos = element_type) %>%
                                  rename(pts = event_points) %>%
                                  rename(total = total_points)
                                                                       )
+      fpl$availablePlayers <- fplR::playerCount(fpl$teamsTable, fpl$all)
     })
   })
   
@@ -78,29 +92,85 @@ server <- function(input, output) {
     rowID <- input$leagueTable_row_last_clicked
     teamID <- fpl$league$standings$results[rowID, 'entry']
     team_table <- fpl$teamsTable[[paste0('T_', teamID)]]
-    team_table$Captaincy <- NA
-    team_table[which(team_table$is_captain), 'Captaincy'] <- 'C'
-    team_table[which(team_table$is_vice_captain), 'Captaincy'] <- 'V'
+    team_table$C <- NA
+    team_table[which(team_table$is_captain), 'C'] <- 'C'
+    team_table[which(team_table$is_vice_captain), 'C'] <- 'V'
     team_table$name <- paste(team_table$first_name, team_table$second_name)
-    team_table <- team_table[, c(1:7, 13, 8:12)]
+#    team_table <- team_table[, c(1:7, 13, 8:12)]
+    team_table <- team_table[, c(1, 3:7, 2, 13, 8:12)]
     
     as.datatable(
       formattable(team_table, 
-                  list(first_name = formatter('span', style = ~ style(color=ifelse(is_captain == TRUE, 'green', 
-                                                                                   ifelse(is_vice_captain == TRUE, 'red', 'black')))),
-                       second_name = formatter('span', style = ~ style(color=ifelse(is_captain == TRUE, 'green', 
-                                                                                         ifelse(is_vice_captain == TRUE, 'red', 'black')))),
-                       Captaincy = formatter('span', style = x ~ style(color = ifelse(x == 'C', "green", "blue")),
-                                             x ~ icontext(ifelse(x == 'C', 'user', 'user')))
-                       )
+#                   list(first_name = formatter('span', style = ~ style(color=ifelse(is_captain == TRUE, 'green', 
+#                                                                                    ifelse(is_vice_captain == TRUE, 'red', 'black')))),
+#                        second_name = formatter('span', style = ~ style(color=ifelse(is_captain == TRUE, 'green', 
+#                                                                                          ifelse(is_vice_captain == TRUE, 'red', 'black')))),
+                       list(C = formatter('span', style = x ~ style(color = ifelse(x == 'C', "red", "green")),
+                                             x ~ icontext(ifelse(x == 'C', 'user', 'user'))),
+                       P = formatter('span', style = x ~ style(color = ifelse(x <= 11, "green", "red")),
+                                             x ~ icontext(ifelse(x <= 11, "ok", "remove")))
+                    )
                   ), 
-      options = list(columnDefs = list(list(visible = FALSE, targets = c(0:6))),
+      options = list(columnDefs = list(list(visible = FALSE, targets = c(0:5))),
                      dom = 't',
                      paging = FALSE,
                      ordering = FALSE,
                      info = FALSE), 
       selection = 'none',
       rownames = FALSE)
+  })
+  
+  output$playerList <- DT::renderDataTable({
+    req(fpl$availablePlayers)
+    df <- fpl$availablePlayers %>% 
+      mutate(player = paste(first_name, second_name)) %>%
+      filter(element_type %in% input$selPosition) %>%
+      rename(available = count) %>%
+      rename(cost = now_cost) %>%
+      rename(total = total_points) %>%
+      rename(pos = element_type) %>% 
+      select(available, player, pos, team, cost, total)
+    
+    df$avail <- apply(df, 1, function(x) ifelse(x['available'] > 0,
+                                             as.character(tags$span(icon('remove', lib = 'glyphicon'), style = 'color:red')),
+                                             as.character(tags$span(icon('ok', lib = 'glyphicon'), style = 'color:green'))))
+    
+    if(input$chkAvailable) df <- df[df$available == 0, ]
+    
+    df <- df[, c(7, 2:6)]
+    
+    DT::datatable(df,
+                        extensions = 'Scroller',
+                        options = list(paging = TRUE,
+                                       deferRender = TRUE,
+                                       scrollY = 400,
+                                       scroller = TRUE
+#                                        columnDefs = list(list(targets = 0, render = JS(
+#                                          "function(data, type, row, meta) { return( data == 0 ? '<span>a</span>' : '<span>X</span>' ) }"
+#                                            
+#                                          
+#                                        )))
+                                       ),
+                        selection = 'none',
+                        rownames = FALSE,
+                  escape = FALSE)
+                  
+
+#     as.datatable(
+#       formattable(
+#         df, list(
+#           available = formatter('span', style = x ~ style(color = ifelse(x == 0, "green", "red")),
+#                                 x ~ icontext(ifelse(x == 0, 'ok-sign', 'remove-sign')))
+#           
+#         )
+#       ),
+#       extensions = 'Scroller',
+#       options = list(paging = TRUE,
+#                      deferRender = TRUE,
+#                      scrollY = 400,
+#                      scroller = TRUE),
+#       selection = 'none',
+#       rownames = FALSE)
   })
   
 }
@@ -112,7 +182,15 @@ ui <- fluidPage(
              column(7, DT::dataTableOutput('leagueTable')),
              column(5, div(DT::dataTableOutput('teamTable'), style = "font-size:80%"))
     ),
-    tabPanel('Available Players')
+    tabPanel('Available Players',
+             fluidRow(
+               column(4, selectizeInput('selPosition', 'Filter by Position', choices = c('GLK', 'DEF', 'MID', 'FWD'), selected = c('GLK', 'DEF', 'MID', 'FWD'), multiple = TRUE)),
+               column(2, checkboxInput('chkAvailable', 'Only Available Players', value = TRUE))
+             ),
+             column(8,
+                    DT::dataTableOutput('playerList')
+             )
+             )
   )
 )
 
